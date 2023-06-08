1bb298122389a605981433df533bf548e3d515cc
mediasphere <mediasphere@192.168.1.5>
2023-06-08 19:32:49 +1000

    Commit 5

--- MavenClass/src/main/java/abstraction/C1.java
+++ MavenClass/src/main/java/abstraction/C1.java
@@ -6,12 +6,17 @@ void show()
 {
 	System.out.println("Morning");
 }
+void print()
+{
+	System.out.println("Non Abstract Method");
+}
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 		C1 obj=new C1();
 		obj.display();
 		obj.show();
+		obj.print();
 
 	}
 
