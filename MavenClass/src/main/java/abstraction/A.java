55235a69b3a88b2162dfa15cd87560ab8d413b0d
mediasphere <mediasphere@192.168.1.5>
2023-06-08 19:05:01 +1000

    test 2

---
+++ MavenClass/src/main/java/abstraction/A.java
@@ -0,0 +1,6 @@
+package abstraction;
+
+abstract class A 
+{
+abstract void display();
+}
---
+++ MavenClass/src/main/java/abstraction/B.java
@@ -0,0 +1,17 @@
+package abstraction;
+
+class B extends A
+{
+void display()
+{
+	System.out.println("Hellow World");
+}
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		B obj=new B();
+		obj.display();
+
+	}
+
+}
